be55b7f0fd7aa34959fa89e984a876e7f7c8e46f
Peter Harper <peter.harper@raspberrypi.com>
2024-05-02 18:57:43 +0100

    Reading the temperature from TMP36

---
+++ adc/hello_adc/hello_tmp36.c
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+int main() {
+    stdio_init_all();
+    printf("ADC Example, Reading from tmp36\n");
+
+    adc_init();
+
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(0);
+
+    while (1) {
+        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+        const float conversion_factor = 3.3f / (1 << 12);
+        uint16_t result = adc_read();
+        float mv = result * conversion_factor * 1000;
+        float temp = (mv - 500) / 10;
+        printf("Raw value: 0x%03x, voltage: %f mV temp: %.2f degC\n", result, mv, temp);
+        sleep_ms(500);
+    }
+}
