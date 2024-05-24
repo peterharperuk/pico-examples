b95c6c2ad04794e055b997a6d013f645085fb8f2
Peter Harper <peter.harper@raspberrypi.com>
2024-05-24 13:55:05 +0100

    Improve hello_gpout a bit
    Add comments and change RTC clock to be the same as ROSC

--- clocks/hello_gpout/hello_gpout.c
+++ clocks/hello_gpout/hello_gpout.c
@@ -14,10 +14,11 @@ int main() {
     printf("Hello gpout\n");
 
     // Output clk_sys / 10 to gpio 21, etc...
-    clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10);
-    clock_gpio_init(23, CLOCKS_CLK_GPOUT1_CTRL_AUXSRC_VALUE_CLK_USB, 10);
-    clock_gpio_init(24, CLOCKS_CLK_GPOUT2_CTRL_AUXSRC_VALUE_CLK_ADC, 10);
-    clock_gpio_init(25, CLOCKS_CLK_GPOUT3_CTRL_AUXSRC_VALUE_CLK_RTC, 10);
+    // Note: Only these GPIOs can be used for clock output, See the GPIO function table in the RP2040 datasheet.
+    clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10); // 12.5MHz
+    clock_gpio_init(23, CLOCKS_CLK_GPOUT1_CTRL_AUXSRC_VALUE_CLK_USB, 10); // 4.8MHz
+    clock_gpio_init(24, CLOCKS_CLK_GPOUT2_CTRL_AUXSRC_VALUE_CLK_ADC, 10); // 4.8MHz
+    clock_gpio_init(25, CLOCKS_CLK_GPOUT3_CTRL_AUXSRC_VALUE_CLK_RTC, 1); // 46875Hz
 
     return 0;
 }
