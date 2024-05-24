54c0a0ce304fe0988e4152efc529601c2bbc7625
Peter Harper <peter.harper@raspberrypi.com>
2024-05-24 11:55:14 +0100

    Minor changes to dormant example

--- sleep/hello_dormant/hello_dormant.c
+++ sleep/hello_dormant/hello_dormant.c
@@ -8,6 +8,8 @@
 #include "pico/stdlib.h"
 #include "pico/sleep.h"
 
+#define WAKE_GPIO 10
+
 int main() {
     stdio_init_all();
     printf("Hello Dormant!\n");
@@ -23,11 +25,11 @@ int main() {
         printf("Running from XOSC\n");
         uart_default_tx_wait_blocking();
 
-        printf("XOSC going dormant\n");
+        printf("XOSC going dormant until GPIO %d goes high\n", WAKE_GPIO);
         uart_default_tx_wait_blocking();
 
         // Go to sleep until we see a high edge on GPIO 10
-        sleep_goto_dormant_until_edge_high(10);
+        sleep_goto_dormant_until_edge_high(WAKE_GPIO);
 
         //Re-enabling clock sources and generators.
         sleep_power_up();
